5013c2cb04063c6ba5245004deb0a97ab5add8f1
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-10 18:12:39 +0400

    Initial Commit

---
+++ Cryptos/CryptosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptosApp.swift
+//  Cryptos
+//
+//  Created by Artem Tebenkov on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
