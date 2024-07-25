f8bf4bdcd9f9a22e375a198b381392a912479d9d
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-25 20:32:20 +0400

    Create launch screen

--- Cryptos/App/CryptosApp.swift
+++ Cryptos/App/CryptosApp.swift
@@ -10,14 +10,27 @@ import SwiftUI
 @main
 struct CryptosApp: App {
 
+    @ObservedObject private var homeViewModel = HomeViewModel()
+    @State var showLaunchView: Bool = true
+
     init() {
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
     }
 
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                HomeView(viewModel: HomeViewModel())
+            ZStack {
+
+                NavigationView {
+                    HomeView(viewModel: homeViewModel)
+                }
+                ZStack {
+                if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                        .transition(AnyTransition.opacity.animation(.easeInOut(duration: 0.3)))
+                    }
+                }
+                .zIndex(2.0)
             }
         }
     }
